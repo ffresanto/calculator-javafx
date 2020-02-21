75d1b811758acd1ee06c6c51f9c1de9f8095dd63
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-02-20 22:38:36 -0300

    Form criado

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,4 +1,27 @@
 package gui;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
 public class ViewController {
+
+    @FXML
+    public JFXButton btn0;
+    public JFXButton btn00;
+    public JFXButton btnPonto;
+    public JFXButton btn1;
+    public JFXButton btn2;
+    public JFXButton btn3;
+    public JFXButton btn4;
+    public JFXButton btn5;
+    public JFXButton btn6;
+    public JFXButton btn7;
+    public JFXButton btn8;
+    public JFXButton btn9;
+    public JFXButton btnSoma;
+    public JFXButton btnSub;
+    public JFXButton btnDividir;
+    public JFXButton btnMultiplicar;
+    public JFXButton btnCancelar;
+    public JFXButton btnCalcular;
 }
